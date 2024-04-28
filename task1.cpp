e98e7e6e9e4e89b8a4bab88d2355f3f9587dfb8d
rites79 <166910186+rites79@users.noreply.github.com>
2024-04-28 12:30:08 +0530

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    // Seed the random number generator
+    
     srand(static_cast<unsigned int>(time(0)));
 
     // Generate a random number between 1 and 100
